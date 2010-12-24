97bc6dc41094942e52614565825c543bc6dba246
Dorneles Tremea <dorneles@x3ng.com.br>
2010-12-24 19:55:04 +0000

    Made sure to add a blank line to fix reST error.
    svn path=/plone.app.referenceablebehavior/trunk/; revision=46501

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '0.1dev'
 setup(name='plone.app.referenceablebehavior',
       version=version,
       description="Referenceable dexterity type behavior",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.txt").read() + "\n\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
