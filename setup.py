7f0f0ab10d1ecfd69fe5b9fe7e83875b4f5993a4
Maurits van Rees <maurits@vanrees.org>
2016-02-09 22:08:30 +0100

    Added classifiers in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -1,48 +1,51 @@
 from setuptools import setup, find_packages
-import os
 
 version = '0.7.4.dev0'
 
-setup(name='plone.app.referenceablebehavior',
-      version=version,
-      description="Referenceable dexterity type behavior",
-      long_description=open("README.rst").read() + "\n\n" +
-                       open("CHANGES.rst").read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+setup(
+    name='plone.app.referenceablebehavior',
+    version=version,
+    description="Referenceable dexterity type behavior",
+    long_description=(open("README.rst").read() + "\n\n" +
+                      open("CHANGES.rst").read()),
+    # Get more strings from
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
         "Framework :: Plone",
+        "Framework :: Plone :: 4.3",
+        "Framework :: Plone :: 5.0",
         "Programming Language :: Python",
+        'Programming Language :: Python :: 2.6',
+        "Programming Language :: Python :: 2.7",
         "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-      keywords='dexterity referenceable plone',
-      author='Plone Foundation',
-      author_email='mailto:dexterity-development@googlegroups.com',
-      url='http://plone.org/products/dexterity',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['plone', 'plone.app'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'plone.behavior',
-          'plone.dexterity >= 1.1',
-          'plone.indexer',
-          'plone.uuid',
-          'Products.Archetypes',
-          # -*- Extra requirements: -*-
-      ],
-      extras_require={
-        'test': ['Products.CMFPlone',
-                 'Products.Archetypes',
-                 'plone.app.testing',
-                 'plone.app.dexterity'],
-      },
-      entry_points="""
-      # -*- Entry points: -*-
+    ],
+    keywords='dexterity referenceable plone',
+    author='Plone Foundation',
+    author_email='mailto:dexterity-development@googlegroups.com',
+    url='http://plone.org/products/dexterity',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['plone', 'plone.app'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'plone.behavior',
+        'plone.dexterity >= 1.1',
+        'plone.indexer',
+        'plone.uuid',
+        'Products.Archetypes',
+    ],
+    extras_require={
+      'test': ['Products.CMFPlone',
+               'Products.Archetypes',
+               'plone.app.testing',
+               'plone.app.dexterity'],
+    },
+    entry_points="""
+    # -*- Entry points: -*-
 
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+    )
